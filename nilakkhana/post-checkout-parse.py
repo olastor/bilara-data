4d8072990608ec49805803571585674ae457c45a
dayunbao <11196050+dayunbao@users.noreply.github.com>
2021-03-20 14:15:38 +0100

    Final prep touches before testing

--- nilakkhana/post-checkout-parse.py
+++ nilakkhana/post-checkout-parse.py
@@ -1,19 +1,14 @@
+import argparse
 import json
 from pathlib import Path
 from typing import Dict
 
-from tqdm import tqdm
-
 from parser import parse
 
-WORK_DIR = Path(__file__).parent.parent
-
-TARGET_FOLDERS = (
-    WORK_DIR / 'root',
-    WORK_DIR / 'translation',
-    WORK_DIR / 'comment',
-)
-
+# Set up argparse to get the files passed by pre-commit
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-f', '--files', required=True, type=Path, nargs='*')
+args = arg_parser.parse_args()
 
 def update_file_content(file_content: Dict[str, str]) -> Dict[str, str]:
     for key, value in file_content.items():
@@ -24,15 +19,15 @@ def update_file_content(file_content: Dict[str, str]) -> Dict[str, str]:
 
 
 if __name__ == '__main__':
-    for folder in TARGET_FOLDERS:
-        print(f'{folder.name} folder in progress')
-        for file in tqdm(folder.glob('**/*.json')):
-            with open(file, 'r+', encoding='utf-8') as target_file:
-                data = json.load(target_file)
-                data = update_file_content(data)
-                target_file.seek(0)
-                target_file.truncate()
-                json.dump(data, target_file, indent=2, ensure_ascii=False)
-        print(f'{folder.name} done!')
+    changed_files = args.files
+    for file in changed_files:
+        print(f'{file} folder in progress')
+        with open(file, 'r+', encoding='utf-8') as target_file:
+            data = json.load(target_file)
+            data = update_file_content(data)
+            target_file.seek(0)
+            target_file.truncate()
+            json.dump(data, target_file, indent=2, ensure_ascii=False)
+        print(f'{file} done!')
 
     print('\nNilakkhana done!')
